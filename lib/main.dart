2c95df0bcae6801f31cf40d9949453963e7a981a
Dennis Paulsen <dennis.paulsen@telekom.de>
2023-12-08 03:23:07 +0100

    first try of implementing SideBar + Row

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,22 @@ void main() async {
   ));
 }
 
+  PageController pageController = PageController();
+  SideMenuController sideMenu = SideMenuController();
+
 class Home extends StatelessWidget {
   const Home({super.key});
+
+
+
+  // @override
+  // void initState() {
+  //   // Connect SideMenuController and PageController together
+  //   sideMenu.addListener((index) {
+  //     pageController.jumpToPage(index);
+  //   });
+  //   super.initState();
+  // }
 
   // This widget is the root of your application.
   @override
@@ -28,7 +42,76 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.deepOrange[500],
         ),
-        body: Center(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SideMenu(
+              // Page controller to manage a PageView
+              controller: sideMenu,
+              // Will shows on top of all items, it can be a logo or a Title text
+              title: Image.asset('assets/todo.png'),
+              // Will show on bottom of SideMenu when displayMode was SideMenuDisplayMode.open
+              footer: Text('demo'),
+              // Notify when display mode changed
+              onDisplayModeChanged: (mode) {
+                print(mode);
+              },
+              // List of SideMenuItem to show them on SideMenu
+              items: items,
+            ),
+            Expanded(
+              child: PageView(
+                controller: pageController,
+                children: [
+                  Container(
+                    child: Center(
+                      child: Text('Dashboard'),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('Settings'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Text('Welcome to the homepage',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrange,
+                fontFamily: 'RubikBubbles',
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                log('Yes sir');
+              },
+              icon: const Icon(Icons.plus_one),
+              label: const Text('Add a new list'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.amber;
+                    }else if (states.contains(MaterialState.hovered)){
+                      return Colors.cyan;
+                    }
+                    return null; // Use the component's default.
+                  },
+                ),
+              ),
+            ),
+            const Image(
+            image: AssetImage('assets/todo.jpg'),
+            fit: BoxFit.fill,
+            ),
+          ],
+        ),
+        
+        
+        //Center(
           // child: Text('Welcome to the homepage',
           //   style: TextStyle(
           //     fontSize: 20.0,
@@ -40,26 +123,26 @@ class Home extends StatelessWidget {
           //   image: AssetImage('assets/todo.jpg'),
           //   fit: BoxFit.fill,
           // ),
-          child: ElevatedButton.icon(
-            onPressed: () {
-              log('Yes sir');
-            },
-            icon: const Icon(Icons.plus_one),
-            label: const Text('Add a new list'),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.resolveWith<Color?>(
-                (Set<MaterialState> states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Colors.amber;
-                  }else if (states.contains(MaterialState.hovered)){
-                    return Colors.cyan;
-                  }
-                  return null; // Use the component's default.
-                },
-              ),
-            ),
-          ),
-        ),
+          // child: ElevatedButton.icon(
+        //     onPressed: () {
+        //       log('Yes sir');
+        //     },
+        //     icon: const Icon(Icons.plus_one),
+        //     label: const Text('Add a new list'),
+        //     style: ButtonStyle(
+        //       backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+        //         (Set<MaterialState> states) {
+        //           if (states.contains(MaterialState.pressed)) {
+        //             return Colors.amber;
+        //           }else if (states.contains(MaterialState.hovered)){
+        //             return Colors.cyan;
+        //           }
+        //           return null; // Use the component's default.
+        //         },
+        //       ),
+        //     ),
+        //   ),
+        // ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('+'),
@@ -68,4 +151,28 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+List<SideMenuItem> items = [
+  SideMenuItem(
+    title: 'Dashboard',
+    onTap:(index, sideMenuController) {
+    },
+    icon: Icon(Icons.home),
+    badgeContent: Text(
+      '3',
+      style: TextStyle(color: Colors.white),
+    ),
+  ),
+  SideMenuItem(
+    title: 'Settings',
+    onTap: (index, sideMenuController) {
+    },
+    icon: Icon(Icons.settings),
+  ),
+  SideMenuItem(
+    title: 'Exit',
+    onTap: (index, sideMenuController) {},
+    icon: Icon(Icons.exit_to_app),
+  ),
+];
 
