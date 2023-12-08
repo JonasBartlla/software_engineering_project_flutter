28fc2a6f57f63f13e148ca32fc8a3bb549d4a602
Dennis Paulsen <dennis.paulsen@telekom.de>
2023-12-08 03:02:43 +0100

    testing button / Images

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,62 @@
+import 'dart:developer';
+import 'dart:html';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:software_engineering_project_flutter/firebase_options.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
 
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+  home: Home(),
+
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: const Text('CheckIT'),
           centerTitle: true,
           backgroundColor: Colors.deepOrange[500],
         ),
-        body: const Center(
-          child: Text('Shababs botten',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.deepOrange,
-              fontFamily: 'RubikBubbles',
+        body: Center(
+          // child: Text('Welcome to the homepage',
+          //   style: TextStyle(
+          //     fontSize: 20.0,
+          //     fontWeight: FontWeight.bold,
+          //     color: Colors.deepOrange,
+          //     fontFamily: 'RubikBubbles',
+          //   ),
+          // child: Image(
+          //   image: AssetImage('assets/todo.jpg'),
+          //   fit: BoxFit.fill,
+          // ),
+          child: ElevatedButton.icon(
+            onPressed: () {
+              log('Yes sir');
+            },
+            icon: const Icon(Icons.plus_one),
+            label: const Text('Add a new list'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return Colors.amber;
+                  }else if (states.contains(MaterialState.hovered)){
+                    return Colors.cyan;
+                  }
+                  return null; // Use the component's default.
+                },
+              ),
             ),
           ),
         ),
@@ -37,7 +65,6 @@ class MyApp extends StatelessWidget {
           child: Text('+'),
           backgroundColor: Colors.deepOrange[500],
       ),
-     ),
     );
   }
 }
