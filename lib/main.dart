5bdfaa74eedacf1619c44c6a3dffd8df71f62e16
Jonas Bartl <jonas.bartl@edu.fhdw.de>
2023-12-13 23:15:27 +0100

    FigmaToFlutter getestet

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,81 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.deepOrange[500],
         ),
-        body: const Center(
-          child: Text('Shababs botten',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.deepOrange,
-              fontFamily: 'RubikBubbles',
-            ),
+        body: Container(
+      decoration: BoxDecoration(
+        color: Colors.black,    
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[Text('E-Mail Adresse', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'Comfortaa',
+        fontSize: 14,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),), SizedBox(height : 7),
+Container(
+        width: 270,
+        height: 27,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(80),
+            topRight: Radius.circular(80),
+            bottomLeft: Radius.circular(80),
+            bottomRight: Radius.circular(80),
           ),
-        ),
+      color : Color.fromRGBO(63, 63, 63, 1),
+  )
+      ),
+],
+      ),
+    ), SizedBox(height : 19),
+Container(
+      decoration: BoxDecoration(
+          
+  ),
+      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        
+        children: <Widget>[Text('Passwort', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'Comfortaa',
+        fontSize: 14,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),), SizedBox(height : 15),
+Container(
+        width: 270,
+        height: 27,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(80),
+            topRight: Radius.circular(80),
+            bottomLeft: Radius.circular(80),
+            bottomRight: Radius.circular(80),
+          ),
+      color : Color.fromRGBO(63, 63, 63, 1),
+  )
+      ),
+],
+      ),
+    ),
+],
+      ),
+    ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('+'),
@@ -41,5 +106,6 @@ class MyApp extends StatelessWidget {
      ),
     );
   }
+  
 }
 
