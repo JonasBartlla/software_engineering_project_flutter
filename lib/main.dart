03be410ac78896d3ad7bbbf78739989c2a018b60
Dennis Paulsen <dennis.paulsen@edu.fhdw.de>
2023-12-16 21:19:24 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'dart:html';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:software_engineering_project_flutter/Task.dart';
 import 'package:software_engineering_project_flutter/firebase_options.dart';
-import 'task.dart';
+
+
+
+
 
 void main() async {
   await Firebase.initializeApp(
@@ -32,6 +36,8 @@ class _HomeState extends State<Home> {
     Task('Sheesh', Color.fromARGB(255, 204, 255, 18), 70.0)
   ];
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,21 +52,56 @@ class _HomeState extends State<Home> {
         ),
         body: Column(
           children: tasks.map((task) {
-              return Container(
-                height: task.height,
-                decoration: BoxDecoration(
-                  color: task.color,
-                  border: Border(
-                    bottom: BorderSide(
-                      style: BorderStyle.solid
-                    )
-                  ),
-                ),
-                child: Center(
-                  child: Text(task.name,) ),
-              );
+              return NewTask(task);
+              // return Container(
+              //   height: task.height,
+              //   decoration: BoxDecoration(
+              //     color: task.color,
+              //     border: Border(
+              //       bottom: BorderSide(
+              //         style: BorderStyle.solid
+              //       )
+              //     ),
+              //   ),
+              //   child: Center(
+              //     child: Text(task.name,) ),
+              // );
             }).toList(),
         ),
+    );
+  }
+}
+
+class NewTask extends StatelessWidget {
+
+  final Task task;
+  // ignore: use_key_in_widget_constructors
+  NewTask(this.task){
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Column(
+        children: <Widget>[
+          Text(task.name,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.grey,
+
+            ),
+          ),
+          SizedBox(height: 6.0,),
+          Text(task.name,
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.blue
+            ),
+          )
+        ]
+      ),
     );
   }
 }
