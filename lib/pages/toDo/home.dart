adb31d81ecbad9c6d108aa1985f2c32476e2b0ae
Dennis Paulsen <dennis.paulsen@edu.fhdw.de>
2024-01-01 19:17:17 +0100

    adding dummy HomeScreen

--- lib/pages/toDo/home.dart
+++ lib/pages/toDo/home.dart
@@ -1,5 +1,12 @@
 import 'dart:html';
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget{
+    @override
+    Widget build(BuildContext context){
+      return Container(
+        child: Text('Home'),
+      );
+    }
 
-class Home{
-  
+
 }
