c8e31d14c5c92ca28127dde6f89ea2c454c7be08
Dennis Paulsen <dennis.paulsen@edu.fhdw.de>
2024-01-29 02:03:45 +0100

    Add ListView Builder

--- lib/pages/home/list_list.dart
+++ lib/pages/home/list_list.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:software_engineering_project_flutter/models/list.dart';
+import 'package:software_engineering_project_flutter/models/listTileTest.dart';
+import 'package:software_engineering_project_flutter/shared/loading.dart';
 
 class ListOfTaskLists extends StatefulWidget {
   const ListOfTaskLists({super.key});
@@ -18,13 +20,15 @@ class _ListOfTaskListsState extends State<ListOfTaskLists> {
   Widget build(BuildContext context) {
 
     final lists = Provider.of<List<TaskList>?>(context);
-    print("a");
-    if (lists != null){
-      for (var taskList in lists){
-        print("b");
-        print(taskList.bezeichnung + " " + taskList.listReference.toString() + " " + taskList.taskCounter.toString());
-      }
-    }
-    return const Placeholder();
+
+    return  lists == null ? Loading() : ListView.builder(
+      shrinkWrap: true,
+      physics: BouncingScrollPhysics(),
+      itemCount: lists.length,
+      itemBuilder: (context, index) {
+        return ListTileTest(taskList: lists[index]);
+      },
+
+    );
   }
 }
