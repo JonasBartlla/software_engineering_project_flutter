f2c94cf8c2f124025715e5416d4e497c77a280bc
Jonas Bartl <jonas.bartl@edu.fhdw.de>
2024-02-18 19:27:19 +0100

    Edited the Frontend of View Tasks

--- lib/models/task_tile.dart
+++ lib/models/task_tile.dart
@@ -24,87 +24,99 @@ class _TaskTileState extends State<TaskTile> {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
       child: Card(
+        color: const Color.fromRGBO(40, 40, 40, 1),
+        elevation: 0,
         margin: const EdgeInsets.fromLTRB(20, 6, 20, 0),
-        child: ListTile(
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: ((context) => EditTodo(task: widget.task))));
-          },
-          leading: Checkbox(
-            value: widget.task.done,
-            shape: const CircleBorder(),
-            checkColor: const Color.fromRGBO(101, 167, 101, 1),
-            activeColor: const Color.fromRGBO(101, 167, 101, 1),
-            onChanged: (bool? isChecked) {
-              setState(() {
-                if (widget.task.done) {
-                  _database.editTask(
-                      widget.task.description,
-                      widget.task.note,
-                      widget.task.maturityDate,
-                      widget.task.notificationOn,
-                      widget.task.priority,
-                      widget.task.list,
-                      false,
-                      widget.task.taskReference);
-                  widget.task.done = false;
-                } else {
-                  _database.editTask(
-                      widget.task.description,
-                      widget.task.note,
-                      widget.task.maturityDate,
-                      widget.task.notificationOn,
-                      widget.task.priority,
-                      widget.task.list,
-                      true,
-                      widget.task.taskReference);
-                  widget.task.done = true;
-                }
-              });
-            },
-          ),
-          // IconButton(
-          //   icon: Icon(Icons.circle_outlined),
-          //   onPressed: (){
-          //     if(widget.task.done){
-          //       _database.editTask(widget.task.description, widget.task.note, widget.task.maturityDate, widget.task.notificationOn, widget.task.priority, widget.task.list, false, widget.task.taskReference);
-          //     } else{
-          //       _database.editTask(widget.task.description, widget.task.note, widget.task.maturityDate, widget.task.notificationOn, widget.task.priority, widget.task.list, true, widget.task.taskReference);
-          //     }
-          //   },
-          // ),
-          title: Text(widget.task.description),
-          subtitle: Row(children: [
-            //Hier die Optionalen Angaben unter der Bezeichnung (hier am besten mit Row in Row arbeiten)
+        child: Column(
+          children: [
+            ListTile(
+              hoverColor: const Color.fromRGBO(40, 40, 40, 1),
+              titleTextStyle: TextStyle(color: Colors.white),
+              subtitleTextStyle: TextStyle(color: Colors.white),
+              tileColor: const Color.fromRGBO(40, 40, 40, 1),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: ((context) => EditTodo(task: widget.task))));
+              },
+              leading: Checkbox(
+                value: widget.task.done,
+                shape: const CircleBorder(),
+                checkColor: const Color.fromRGBO(101, 167, 101, 1),
+                activeColor: const Color.fromRGBO(101, 167, 101, 1),
+                side: BorderSide(color: Colors.white),
+                onChanged: (bool? isChecked) {
+                  setState(() {
+                    if (widget.task.done) {
+                      _database.editTask(
+                          widget.task.description,
+                          widget.task.note,
+                          widget.task.maturityDate,
+                          widget.task.notificationOn,
+                          widget.task.priority,
+                          widget.task.list,
+                          false,
+                          widget.task.taskReference);
+                      widget.task.done = false;
+                    } else {
+                      _database.editTask(
+                          widget.task.description,
+                          widget.task.note,
+                          widget.task.maturityDate,
+                          widget.task.notificationOn,
+                          widget.task.priority,
+                          widget.task.list,
+                          true,
+                          widget.task.taskReference);
+                      widget.task.done = true;
+                    }
+                  });
+                },
+              ),
+              // IconButton(
+              //   icon: Icon(Icons.circle_outlined),
+              //   onPressed: (){
+              //     if(widget.task.done){
+              //       _database.editTask(widget.task.description, widget.task.note, widget.task.maturityDate, widget.task.notificationOn, widget.task.priority, widget.task.list, false, widget.task.taskReference);
+              //     } else{
+              //       _database.editTask(widget.task.description, widget.task.note, widget.task.maturityDate, widget.task.notificationOn, widget.task.priority, widget.task.list, true, widget.task.taskReference);
+              //     }
+              //   },
+              // ),
+              title: Text(widget.task.description),
+              subtitle: Row(children: [
+                //Hier die Optionalen Angaben unter der Bezeichnung (hier am besten mit Row in Row arbeiten)
 
-            //Priorität
-            widget.task.priority == 'no priority'
-                ? const Text('')
-                : const Icon(Icons.arrow_upward),
-            Text(widget.task.priority),
+                //Priorität
+                widget.task.priority == 'no priority'
+                    ? const Text('')
+                    : const Icon(Icons.arrow_upward, color: Colors.white),
+                Text(widget.task.priority, style: const TextStyle(color: Colors.white)),
 
-            //Datum und Uhrzeit
-            widget.task.maturityDate == DateTime.fromMillisecondsSinceEpoch(0)
-                ? const Text('')
-                : const Icon(Icons.calendar_month_rounded),
-            Text(widget.task.maturityDate.toString()),
-            const SizedBox(
-              width: 4,
+                //Datum und Uhrzeit
+                widget.task.maturityDate == DateTime.fromMillisecondsSinceEpoch(0)
+                    ? const Text('')
+                    : const Icon(Icons.calendar_month_rounded, color: Colors.white,),
+                Text(widget.task.maturityDate.toString(), style: const TextStyle(color: Colors.white),),
+                const SizedBox(
+                  width: 4,
+                ),
+
+                //Notiz
+                widget.task.note == ''
+                    ? const Text('')
+                    : const Icon(Icons.list_alt_rounded, color: Colors.white,),
+
+                //Benachrichtigung
+                widget.task.notificationOn == true
+                    ? const Icon(Icons.notifications_on, color: Colors.white,)
+                    : const Text(''),
+              ]),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.white,),
             ),
-
-            //Notiz
-            widget.task.note == ''
-                ? const Text('')
-                : const Icon(Icons.list_alt_rounded),
-
-            //Benachrichtigung
-            widget.task.notificationOn == true
-                ? const Icon(Icons.notifications_on)
-                : const Text(''),
-          ]),
-          trailing: const Icon(Icons.arrow_forward_ios_rounded),
+            Divider( color: Colors.grey,)
+          ],
         ),
       ),
     );
