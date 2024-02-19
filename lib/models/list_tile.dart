53701a75e2c1ede91b0077b4dee977fcd0ccd979
lucaSchneiderFHDW <145675374+lucaSchneiderFHDW@users.noreply.github.com>
2024-02-19 11:24:27 +0100

    Anpassungen für Farben erfolgt

--- lib/models/list_tile.dart
+++ lib/models/list_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:software_engineering_project_flutter/models/task.dart';
 import 'package:software_engineering_project_flutter/models/task_list.dart';
+import 'package:software_engineering_project_flutter/shared/colors.dart';
 import 'package:software_engineering_project_flutter/pages/home/lists/edit_list_screen.dart';
 import 'package:software_engineering_project_flutter/pages/home/lists/view_tasks_screen.dart';
 import 'package:software_engineering_project_flutter/shared/styles_and_decorations.dart';
@@ -21,7 +22,7 @@ class ListTileTest extends StatelessWidget {
       padding: EdgeInsets.only(top: 8.0),
         child: Card(
           elevation: 4,
-          color: Color.fromRGBO(63, 63, 63, 1),
+          color: AppColors.myTilesColor,
           margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
           child: SizedBox(
             height: 30,
@@ -41,7 +42,7 @@ class ListTileTest extends StatelessWidget {
                 children: [
                   Icon(
                     taskList.icon,
-                    color: const Color.fromRGBO(101, 167, 101, 1),
+                    color: AppColors.myGreenButton,
                     size: 48.0,
                   ),
                   SizedBox(height: 8.0),
