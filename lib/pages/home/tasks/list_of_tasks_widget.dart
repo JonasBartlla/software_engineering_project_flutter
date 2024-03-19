212d5550f9abab64e4b1c2d7d10d903c5067ea0b
Dennis Paulsen <dennis.paulsen@edu.fhdw.de>
2024-03-19 14:19:27 +0100

    fill 3 initialLists with correct Tasks

--- lib/pages/home/tasks/list_of_tasks_widget.dart
+++ lib/pages/home/tasks/list_of_tasks_widget.dart
@@ -14,13 +14,35 @@ class ListOfTasks extends StatefulWidget {
 }
 
 class _ListOfTasksState extends State<ListOfTasks> {
+
+  late List<Task> tasks;
+
   @override
   Widget build(BuildContext context) {
 
     //late List<Task> tasks = widget.tasks;
-    List<Task> tasks = Provider.of<List<Task>>(context).where((element){
-      return element.list == widget.listDescription ? true : false;
-    }).toList();
+    switch(widget.listDescription){
+      case 'Alle ToDos':
+        tasks = Provider.of<List<Task>>(context);
+      case 'Erledigte ToDos':
+        tasks = Provider.of<List<Task>>(context).where((element){
+          return element.done == true;
+        }).toList();
+      case 'Mein Tag':
+        tasks = Provider.of<List<Task>>(context).where((element){
+          DateTime rightNow = DateTime.now();
+          if (element.maturityDate.isAfter(DateTime(rightNow.year,rightNow.month,rightNow.day)) && element.maturityDate.isBefore(DateTime(rightNow.year,rightNow.month,rightNow.day+1))){
+            return true;
+          }else{
+            return false;
+          }
+        }).toList();
+      default:
+        tasks = Provider.of<List<Task>>(context).where((element){
+                  return element.list == widget.listDescription ? true : false;
+                }).toList();
+
+    }
     return tasks == null ? Loading() : ListView.builder(
       shrinkWrap: true,
       physics: const BouncingScrollPhysics(),
