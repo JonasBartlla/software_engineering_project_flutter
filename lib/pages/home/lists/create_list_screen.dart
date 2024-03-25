3dd542e641e51bc09393e353c78b8d3f5a46b271
Jonas Bartl <jonas.bartl@edu.fhdw.de>
2024-03-25 20:43:34 +0100

    modified variables on edit list modified preview Task list on edit and create Task List screen made some const and import changes

--- lib/pages/home/lists/create_list_screen.dart
+++ lib/pages/home/lists/create_list_screen.dart
@@ -96,11 +96,10 @@ class _CreateListPageState extends State<CreateListPage> {
                                 //Bezeichnung eingeben
                                 child: TextFormField(
                                   inputFormatters: [
-                                    LengthLimitingTextInputFormatter(20)
+                                    LengthLimitingTextInputFormatter(16)
                                   ],
                                   style: const TextStyle(
                                       color: AppColors.myTextColor),
-                                  initialValue: "",
                                   validator: (value) {
                                     if (value!.isEmpty) {
                                       return 'Bitte eine Bezeichnung eingeben';
@@ -184,7 +183,7 @@ class _CreateListPageState extends State<CreateListPage> {
                               style: buttonBoxDecoration.copyWith(
                                   backgroundColor:
                                       MaterialStatePropertyAll(iconColor)),
-                              child: SizedBox(height: 10),
+                              child: const SizedBox(height: 10),
                               onPressed: () {
                                 showDialog(
                                     context: context,
@@ -213,8 +212,12 @@ class _CreateListPageState extends State<CreateListPage> {
                                 child: const Text(
                                   'Abbrechen',
                                   style: TextStyle(
-                                    color: Colors.white,
-                                  ),
+                                          color: AppColors.myTextColor,
+                                          fontFamily: 'Comfortaa',
+                                          fontSize: 14,
+                                          letterSpacing: 1,
+                                          fontWeight: FontWeight.normal,
+                                          height: 1)
                                 ),
                               ),
                             ),
@@ -225,7 +228,13 @@ class _CreateListPageState extends State<CreateListPage> {
                               //Erstellen Button
                               child: ElevatedButton(
                                 style: buttonStyleDecorationcolorchange,
-                                child: const Text('Erstellen'),
+                                child: const Text('Erstellen', style: TextStyle(
+                                        color: AppColors.myTextColor,
+                                        fontFamily: 'Comfortaa',
+                                        fontSize: 14,
+                                        letterSpacing: 1,
+                                        fontWeight: FontWeight.normal,
+                                        height: 1),),
                                 onPressed: () {
                                   if (_formKey.currentState!.validate()) {
                                     _database.addList(title, icon, iconColor);
@@ -256,30 +265,95 @@ class _CreateListPageState extends State<CreateListPage> {
                 const SizedBox(
                   height: 20,
                 ),
+                // SizedBox(
+                //   height: 300,
+                //   width: 350,
+                //   child: Card(
+                //     color: AppColors.myCheckITDarkGrey,
+                //     child: Padding(
+                //       padding: const EdgeInsets.all(100),
+                //       child: Column(
+                //         mainAxisAlignment: MainAxisAlignment.center,
+                //         children: [
+                //           Icon(
+                //             icon,
+                //             color: iconColor,
+                //           ),
+                //           const SizedBox(height: 20),
+                //           Text(
+                //             title,
+                //             style: standardHeadlineDecoration,
+                //           )
+                //         ],
+                //       ),
+                //     ),
+                //   ),
+                // ),
                 SizedBox(
-                  height: 300,
-                  width: 350,
-                  child: Card(
-                    color: AppColors.myCheckITDarkGrey,
-                    child: Padding(
-                      padding: const EdgeInsets.all(100),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            icon,
-                            color: iconColor,
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            title,
-                            style: standardHeadlineDecoration,
-                          )
-                        ],
-                      ),
+                  width: 250,
+                  height: 170,
+                   child: Padding(
+                         padding: const EdgeInsets.only(top: 8.0),
+                         child: Card(
+                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                           elevation: 10,
+                           color:  AppColors.myCheckITDarkGrey,
+                           surfaceTintColor:  AppColors.myCheckITDarkGrey,
+                           margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+                           child: SizedBox(
+                             height: 30,
+                             width: 30,
+                             child: Stack(
+                               children: [
+                                 Align(
+                                   alignment: Alignment.center,
+                                   child: ListTile(
+                    contentPadding: const EdgeInsets.all(8.0),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          icon,
+                          color: iconColor,
+                          size: 48.0,
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          title,
+                          style: standardHeadlineDecoration,
+                          textAlign: TextAlign.center,
+                        ),
+                   
+                        const SizedBox(height: 5.0),
+                      ],
                     ),
-                  ),
-                ),
+                                   ),
+                                 ),
+                                 Positioned(
+                                   top: 8.0,
+                                   right: 8.0,
+                                   child: Container(
+                    width: 25,
+                    height: 25,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white),
+                    ),
+                    child: Center(
+                        child: Text(
+                      '1',
+                      style: standardTextDecoration.copyWith(color: Colors.black),
+                    )),
+                                   ),
+                                 )
+                               ],
+                             ),
+                           ),
+                         ),
+                               ),
+                 )
               ],
             ),
           ),
--- lib/pages/home/lists/edit_list_screen.dart
+++ lib/pages/home/lists/edit_list_screen.dart
@@ -4,6 +4,8 @@ import 'package:software_engineering_project_flutter/models/task_list.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:software_engineering_project_flutter/services/databaseService.dart';
+import 'package:software_engineering_project_flutter/shared/color_picker.dart';
+import 'package:software_engineering_project_flutter/shared/icon_picker.dart';
 import 'package:software_engineering_project_flutter/shared/styles_and_decorations.dart';
 import 'package:software_engineering_project_flutter/shared/colors.dart';
 
@@ -24,19 +26,12 @@ class _EditListPageState extends State<EditListPage> {
   //Felder einer Liste
   late String title = taskList.description;
   late IconData icon = taskList.icon;
-
-  //Liste für die auswählbaren Icons
-  List<IconData> choosableIcons = [
-    Icons.abc,
-    Icons.house,
-    Icons.calendar_month_rounded
-  ];
+  late Color iconColor = taskList.iconColor;
   
   @override
   Widget build(BuildContext context) {
     final User? user = Provider.of<User?>(context);
     final DatabaseService _database = DatabaseService(uid: user?.uid);
-    Color iconColor = Colors.white;
     return Scaffold(
       backgroundColor: const Color.fromRGBO(40, 40, 40, 1),
       appBar: AppBar(
@@ -47,7 +42,7 @@ class _EditListPageState extends State<EditListPage> {
         backgroundColor: const Color.fromRGBO(101, 167, 101, 1),
         title: Center(
           child: Text(
-            'Liste erstellen',
+            'Liste bearbeiten',
             style: standardAppBarTextDecoration,
           ),
         ),
@@ -97,11 +92,11 @@ class _EditListPageState extends State<EditListPage> {
                                 //Bezeichnung eingeben
                                 child: TextFormField(
                                   inputFormatters: [
-                                    LengthLimitingTextInputFormatter(20)
+                                    LengthLimitingTextInputFormatter(16)
                                   ],
                                   style: const TextStyle(
                                       color: AppColors.myTextColor),
-                                  initialValue: "",
+                                  initialValue: taskList.description,
                                   validator: (value) {
                                     if (value!.isEmpty) {
                                       return 'Bitte eine Bezeichnung eingeben';
@@ -138,43 +133,90 @@ class _EditListPageState extends State<EditListPage> {
                             const SizedBox(
                               width: 10,
                             ),
-                            SizedBox(
-                              width: 288,
-                              child: DropdownButtonFormField<IconData>(
-                                  decoration: textInputDecoration.copyWith(
-                                      hintText: 'Icon'),
-                                  items: choosableIcons.map((icon) {
-                                    return DropdownMenuItem(
-                                      value: icon,
-                                      child: Icon(icon),
-                                    );
-                                  }).toList(),
-                                  onChanged: (value) => setState(() {
-                                        icon = value!;
-                                      })),
+                            Center(
+                              child: IconButton(
+                                style: buttonBoxDecoration,
+                                color: AppColors.myBoxColor,
+                                icon: Icon(
+                                  icon,
+                                  color: iconColor,
+                                  size: 30,
+                                ),
+                                onPressed: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return IconPickerDialog(
+                                          iconColor: iconColor,
+                                            onIconSelected: (selectedIcon) {
+                                          setState(() {
+                                            icon = selectedIcon;
+                                          });
+                                        });
+                                      });
+                                },
+                              ),
                             ),
+                            const SizedBox(height: 10),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(children: <Widget>[
+                          const SizedBox(width: 8),
+                          const Text(
+                            'Farbe:',
+                            style: TextStyle(
+                                color: AppColors.myTextColor,
+                                fontFamily: 'Comfortaa',
+                                fontSize: 18,
+                                letterSpacing: 1,
+                                fontWeight: FontWeight.normal,
+                                height: 1),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Center(
+                            child: TextButton(
+                              style: buttonBoxDecoration.copyWith(
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(iconColor)),
+                              child: const SizedBox(height: 10),
+                              onPressed: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return ColorPickerDialog(
+                                          onColorSelected: (selectedColor) {
+                                        setState(() {
+                                          iconColor = selectedColor;
+                                        });
+                                      });
+                                    });
+                              },
+                            ),
+                          ),
+                            // SizedBox(
+                            //   width: 288,
+                            //   child: DropdownButtonFormField<IconData>(
+                            //       decoration: textInputDecoration.copyWith(
+                            //           hintText: 'Icon'),
+                            //       items: choosableIcons.map((icon) {
+                            //         return DropdownMenuItem(
+                            //           value: icon,
+                            //           child: Icon(icon),
+                            //         );
+                            //       }).toList(),
+                            //       onChanged: (value) => setState(() {
+                            //             icon = value!;
+                            //           })),
+                            // ),
                           ],
                         ),
                         const SizedBox(height: 45),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            SizedBox(
-                                //Löschen Button
-                                child: TextButton(
-                                  style: buttonStyleDecorationDelete,
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text(
-                                    'Löschen',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(width: 15),
                             SizedBox(
                               //Abbrechen Button
                               child: TextButton(
@@ -184,8 +226,12 @@ class _EditListPageState extends State<EditListPage> {
                                 child: const Text(
                                   'Abbrechen',
                                   style: TextStyle(
-                                    color: Colors.white,
-                                  ),
+                                          color: AppColors.myTextColor,
+                                          fontFamily: 'Comfortaa',
+                                          fontSize: 14,
+                                          letterSpacing: 1,
+                                          fontWeight: FontWeight.normal,
+                                          height: 1),
                                 ),
                               ),
                             ),
@@ -193,10 +239,17 @@ class _EditListPageState extends State<EditListPage> {
                               width: 15,
                             ),
                             SizedBox(
-                              //Bearbeiten Button
+                              //Speichern Button
                               child: ElevatedButton(
                                 style: buttonStyleDecorationcolorchange,
-                                child: const Text('Bearbeiten'),
+                                child: const Text('Speichern', style: TextStyle(
+                                        color: AppColors.myTextColor,
+                                        fontFamily: 'Comfortaa',
+                                        fontSize: 14,
+                                        letterSpacing: 1,
+                                        fontWeight: FontWeight.normal,
+                                        height: 1),
+                                  ),
                                 onPressed: () {
                                   if (_formKey.currentState!.validate()) {
                                     _database.editList(title, icon, iconColor, widget.taskList.listReference, widget.taskList.creationDate, widget.taskList.isEditable, widget.taskList.ownerId);
@@ -227,25 +280,92 @@ class _EditListPageState extends State<EditListPage> {
                 const SizedBox(
                   height: 20,
                 ),
+                //  Padding(
+                //    padding: const EdgeInsets.only(top: 8.0),
+                //    child: Card(
+                //       margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+                //       color: AppColors.myCheckITDarkGrey,
+                //       surfaceTintColor: AppColors.myCheckITDarkGrey,
+                //         child: SizedBox(
+                //           height: 30,
+                //           width: 30,
+                //           child: Column(
+                //             mainAxisAlignment: MainAxisAlignment.center,
+                //             children: [
+                //               Icon(icon, color: iconColor,),
+                //               const SizedBox(height: 20),
+                //               Text(title, style: standardHeadlineDecoration,)
+                //             ],
+                //           ),
+                //         ),                  
+                //     ),
+                //  ),
+
                 SizedBox(
-                  height: 300,
-                  width: 350,
-                  child: Card(
-                    color: AppColors.myCheckITDarkGrey,
-                      child: Padding(
-                        padding: const EdgeInsets.all(100),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(icon, color: AppColors.myCheckItGreen,),
-                            const SizedBox(height: 20),
-                            Text(title, style: standardHeadlineDecoration,)
-                          ],
+                  width: 250,
+                  height: 170,
+                   child: Padding(
+                         padding: const EdgeInsets.only(top: 8.0),
+                         child: Card(
+                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                           elevation: 10,
+                           color:  AppColors.myCheckITDarkGrey,
+                           surfaceTintColor:  AppColors.myCheckITDarkGrey,
+                           margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+                           child: SizedBox(
+                             height: 30,
+                             width: 30,
+                             child: Stack(
+                               children: [
+                                 Align(
+                                   alignment: Alignment.center,
+                                   child: ListTile(
+                    contentPadding: const EdgeInsets.all(8.0),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          icon,
+                          color: iconColor,
+                          size: 48.0,
                         ),
-                      ),                  
-                  ),
-                ),
-              ],
+                        const SizedBox(height: 8.0),
+                        Text(
+                          title,
+                          style: standardHeadlineDecoration,
+                          textAlign: TextAlign.center,
+                        ),
+                   
+                        const SizedBox(height: 5.0),
+                      ],
+                    ),
+                                   ),
+                                 ),
+                                 Positioned(
+                                   top: 8.0,
+                                   right: 8.0,
+                                   child: Container(
+                    width: 25,
+                    height: 25,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white),
+                    ),
+                    child: Center(
+                        child: Text(
+                      '1',
+                      style: standardTextDecoration.copyWith(color: Colors.black),
+                    )),
+                                   ),
+                                 )
+                               ],
+                             ),
+                           ),
+                         ),
+                               ),
+                 )],
             ),
           ),
         ),
