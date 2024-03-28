c6c6d067edf0aa16b613daaabfc46cba8ef29969
lucaSchneiderFHDW <145675374+lucaSchneiderFHDW@users.noreply.github.com>
2024-03-28 19:41:01 +0100

    Anfang des Google Sign In

--- lib/services/authService.dart
+++ lib/services/authService.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 import 'package:software_engineering_project_flutter/models/app_user.dart';
 import 'package:software_engineering_project_flutter/models/custom_user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:software_engineering_project_flutter/services/databaseService.dart';
 
 class AuthService{
