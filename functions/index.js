8441ba6052dc39273e1eaa465d03cfe203ee5356
Dennis Paulsen <dennis.paulsen@edu.fhdw.de>
2024-04-06 16:33:26 +0200

    update index.js to log when task created

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,7 @@
  */
 
 const {onRequest} = require("firebase-functions/v2/https");
+const functions = require("firebase-functions");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
@@ -17,3 +18,22 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.testFunc = functions.region('europe-west3').https.onRequest((request, response) => {
+    logger.info("Hello its me");
+    var abc = request.params[0];
+    response.send("Hello from Dennis" + abc);
+})
+
+exports.notifiCationOnDocCreate = functions.region('europe-west3').firestore
+    .document('tasks/{taskId}')
+    .onCreate(async (snapshot, context) => {
+        const taskId = context.params.taskId;
+        const taskData = snapshot.data();
+        const sender = taskData.ownerId;
+        const name = taskData.description;
+
+        logger.info('Ein neues Dokument wurde erstellt');
+        logger.info('Das ToDo ' + name + ' Dokument wurde von ' + sender + ' erstellt.')
+    }
+);
