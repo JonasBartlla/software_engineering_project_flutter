e5220b0b431f7130120176bed46938f45ca5887c
Dennis Paulsen <dennis.paulsen@edu.fhdw.de>
2024-04-08 03:20:36 +0200

    cloud function final

--- functions/index.js
+++ functions/index.js
@@ -2,22 +2,23 @@ const functions = require("firebase-functions");
 const logger = require("firebase-functions/logger");
 const {getFirestore} = require("firebase-admin/firestore");
 const admin = require('firebase-admin');
+const {onSchedule} = require("firebase-functions/v2/scheduler");
 
 admin.initializeApp();
 
 
-exports.logNotifiCationOnDocCreate = functions.region('europe-west3').firestore
-    .document('tasks/{taskId}')
-    .onCreate(async (snapshot, context) => {
-        const taskId = context.params.taskId;
-        const taskData = snapshot.data();
-        const sender = taskData.ownerId;
-        const name = taskData.description;
+// exports.logNotifiCationOnDocCreate = functions.region('europe-west3').firestore
+//     .document('tasks/{taskId}')
+//     .onCreate(async (snapshot, context) => {
+//         const taskId = context.params.taskId;
+//         const taskData = snapshot.data();
+//         const sender = taskData.ownerId;
+//         const name = taskData.description;
 
-        logger.info('Ein neues Dokument wurde erstellt');
-        logger.info('Das ToDo ' + name + ' Dokument wurde von ' + sender + ' erstellt.')
-    }
-);
+//         logger.info('Ein neues Dokument wurde erstellt');
+//         logger.info('Das ToDo ' + name + ' Dokument wurde von ' + sender + ' erstellt.')
+//     }
+// );
 
 // exports.pushNotifiCationOnDocCreate2 = functions.region('europe-west3').firestore
 //     .document('notification/{notificationId}')
@@ -60,6 +61,59 @@ exports.logNotifiCationOnDocCreate = functions.region('europe-west3').firestore
 
 
 //     });
+
+exports.tt = onSchedule("* * * * *", async (event) => {
+    var currentDate = new Date();
+    logger.info(currentDate.getTime());
+    const querySnapshot = (await getFirestore().collection('notification').where('maturityDate', '<',currentDate.getTime() + (16 * 60 * 1000)).where('messageSent','=',false).get()).docs;
+    logger.info(querySnapshot.length);
+    if(querySnapshot.length >0){
+        querySnapshot.forEach(async (doc) =>{
+            notificationData = doc.data()
+            logger.info(doc.id);
+
+            const taskOwner = notificationData.ownerId; //get uid of owner
+            logger.info('Owned by ' + taskOwner);
+            const ownerToken = (await getFirestore().collection('users').where('uid', '=', taskOwner).get()).docs[0].data().token;
+            logger.info('owner token' + ownerToken);
+            if (ownerToken != ''){
+                const affectedTaskId = notificationData.taskId; //get uid of task
+                logger.info('Task referenced by ' + taskOwner);
+                const taskData = (await getFirestore().collection('tasks').doc(affectedTaskId).get()).data(); //get the data of affected task
+                admin
+                .messaging()
+                .send(
+                    {
+                    token: ownerToken,
+                    data: {
+                        foo: 'bar',
+                        description: taskData.description,
+                        list: taskData.list,
+                        note: taskData.note,
+                        priority: taskData.priority.toString()
+                    },
+                    notification: {
+                        title: 'Ein ToDo wird fällig',
+                        body: 'Ihr ToDo ' + taskData.description + ' wird fällig',
+                    },
+                    },
+                );
+                getFirestore().collection('notification').doc(doc.id).update({'messageSent': true});
+                logger.info('NotificationSend set to true');
+                
+            }else{
+                logger.info("The user disabled notifications no message will be sent");
+            }
+
+
+
+        });
+    }else{
+        logger.info('No notification needs to be sent');
+    }
+
+   
+});
 
 exports.testNot = functions.region('europe-west3').firestore
 .document('notification/{notificationId}')
@@ -112,57 +166,57 @@ exports.testNot = functions.region('europe-west3').firestore
 //     }
 // });
 
-exports.sendNotificationForDueTasks = functions.region('europe-west3').firestore
-.document('notification/{notificationId}')
-.onCreate(async (snapshot, context) => { 
-    var currentDate = new Date();
-    logger.info(currentDate.getTime());
-    const querySnapshot = (await getFirestore().collection('notification').where('maturityDate', '<',currentDate.getTime() + (16 * 60 * 1000)).where('messageSent','=',false).get()).docs;
-    logger.info(querySnapshot.length);
-    if(querySnapshot.length >0){
-        querySnapshot.forEach(async (doc) =>{
-            notificationData = doc.data()
-            logger.info(doc.id);
+// exports.sendNotificationForDueTasks = functions.region('europe-west3').firestore
+// .document('notification/{notificationId}')
+// .onCreate(async (snapshot, context) => { 
+//     var currentDate = new Date();
+//     logger.info(currentDate.getTime());
+//     const querySnapshot = (await getFirestore().collection('notification').where('maturityDate', '<',currentDate.getTime() + (16 * 60 * 1000)).where('messageSent','=',false).get()).docs;
+//     logger.info(querySnapshot.length);
+//     if(querySnapshot.length >0){
+//         querySnapshot.forEach(async (doc) =>{
+//             notificationData = doc.data()
+//             logger.info(doc.id);
 
-            const taskOwner = notificationData.ownerId; //get uid of owner
-            logger.info('Owned by ' + taskOwner);
-            const ownerToken = (await getFirestore().collection('users').where('uid', '=', taskOwner).get()).docs[0].data().token;
-            logger.info('owner token' + ownerToken);
-            if (ownerToken != ''){
-                const affectedTaskId = notificationData.taskId; //get uid of task
-                logger.info('Task referenced by ' + taskOwner);
-                const taskData = (await getFirestore().collection('tasks').doc(affectedTaskId).get()).data(); //get the data of affected task
-                admin
-                .messaging()
-                .send(
-                    {
-                    token: ownerToken,
-                    data: {
-                        foo: 'bar',
-                        description: taskData.description,
-                        list: taskData.list,
-                        note: taskData.note,
-                        priority: taskData.priority.toString()
-                    },
-                    notification: {
-                        title: 'Ein ToDo wird fällig',
-                        body: 'Ihr ToDo ' + taskData.description + ' wird fällig',
-                    },
-                    },
-                );
-                getFirestore().collection('notification').doc(doc.id).update({'messageSent': true});
-                logger.info('NotificationSend set to true');
+//             const taskOwner = notificationData.ownerId; //get uid of owner
+//             logger.info('Owned by ' + taskOwner);
+//             const ownerToken = (await getFirestore().collection('users').where('uid', '=', taskOwner).get()).docs[0].data().token;
+//             logger.info('owner token' + ownerToken);
+//             if (ownerToken != ''){
+//                 const affectedTaskId = notificationData.taskId; //get uid of task
+//                 logger.info('Task referenced by ' + taskOwner);
+//                 const taskData = (await getFirestore().collection('tasks').doc(affectedTaskId).get()).data(); //get the data of affected task
+//                 admin
+//                 .messaging()
+//                 .send(
+//                     {
+//                     token: ownerToken,
+//                     data: {
+//                         foo: 'bar',
+//                         description: taskData.description,
+//                         list: taskData.list,
+//                         note: taskData.note,
+//                         priority: taskData.priority.toString()
+//                     },
+//                     notification: {
+//                         title: 'Ein ToDo wird fällig',
+//                         body: 'Ihr ToDo ' + taskData.description + ' wird fällig',
+//                     },
+//                     },
+//                 );
+//                 getFirestore().collection('notification').doc(doc.id).update({'messageSent': true});
+//                 logger.info('NotificationSend set to true');
                 
-            }else{
-                logger.info("The user disabled notifications no message will be sent");
-            }
+//             }else{
+//                 logger.info("The user disabled notifications no message will be sent");
+//             }
 
 
 
-        });
-    }else{
-        logger.info('No notification needs to be sent');
-    }
+//         });
+//     }else{
+//         logger.info('No notification needs to be sent');
+//     }
 
 
-});
+// });
