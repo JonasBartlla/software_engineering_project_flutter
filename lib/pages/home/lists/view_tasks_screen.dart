0f1872e7ff1ddfde62e515515260ff6c3f51cfab
Dennis Paulsen <dennis.paulsen@edu.fhdw.de>
2024-04-12 00:57:08 +0200

    change that affected task will be updated if listname change

--- lib/pages/home/lists/view_tasks_screen.dart
+++ lib/pages/home/lists/view_tasks_screen.dart
@@ -7,6 +7,7 @@ import 'package:software_engineering_project_flutter/services/databaseService.da
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:software_engineering_project_flutter/models/task.dart';
 import 'package:software_engineering_project_flutter/shared/colors.dart';
+import 'package:software_engineering_project_flutter/shared/loading.dart';
 import 'package:software_engineering_project_flutter/shared/other_functions.dart';
 import 'package:software_engineering_project_flutter/shared/styles_and_decorations.dart';
 import 'package:software_engineering_project_flutter/models/sort_fields.dart';
@@ -38,118 +39,147 @@ class _ListOfTaskPageState extends State<ListOfTasksPage> {
     final User? user = Provider.of<User?>(context);
     final DatabaseService _database = DatabaseService(uid: user?.uid);
 
-    return StreamProvider<List<Task>>.value(
-      initialData: [],
-      value: _database.tasks,
-      child: Scaffold(
-        backgroundColor: AppColors.myBackgroundColor,
-        appBar: AppBar(
-          backgroundColor: AppColors.myCheckItGreen,
-          leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back_ios_new,
-              color: AppColors.myBackgroundColor,
-              size: 35,
-            ),
-            onPressed: () => Navigator.pop(context),
-          ),
-          title: Icon(
-            taskList.icon,
-            color: AppColors.myBackgroundColor,
-            size: 40,
-          ),
-          centerTitle: true,
+    return MultiProvider(
+      providers: [
+        StreamProvider<List<Task>>.value(
+        initialData: [],
+        value: _database.tasks,
         ),
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 8, 8, 8),
-                child: Text(
-                  taskList.description,
-                  style: const TextStyle(
-                      color: AppColors.myTextColor, fontSize: 25),
-                ),
+        StreamProvider<List<TaskList>>.value(
+        initialData: [],
+        value: _database.lists,
+        )
+      ],
+        child: Scaffold(
+          backgroundColor: AppColors.myBackgroundColor,
+          appBar: AppBar(
+            backgroundColor: AppColors.myCheckItGreen,
+            leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+                color: AppColors.myBackgroundColor,
+                size: 35,
               ),
+              onPressed: () => Navigator.pop(context),
             ),
-            const SizedBox(
-              height: 2,
+            title: Icon(
+              taskList.icon,
+              color: AppColors.myBackgroundColor,
+              size: 40,
             ),
-            const SizedBox(
-              height: 5,
-            ),
-            Expanded(
-                child: ListOfTasks(
-              listDescription: widget.taskList.description,
-              lists: widget.lists,
-            )),
-            const SizedBox(
-              height: 5,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                getEditButton(taskList, _database, context),
-                const SizedBox(
-                  width: 25,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _database.getAvailableListForUser().then((lists) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => CreateToDo(
-                                  listCreatedFrom: taskList.description,
-                                  availableLists: lists))));
-                    });
-                    setState(() {});
-                  },
-                  style: ButtonStyle(
-                          padding: MaterialStateProperty.all(EdgeInsets.all(1)),
-                          surfaceTintColor: MaterialStateProperty.all(
-                              AppColors.myCheckItGreen),
-                          overlayColor: MaterialStateProperty.all(
-                              AppColors.myCheckItGreen),
-                          backgroundColor: MaterialStateProperty.all(AppColors.myCheckItGreen),
-                          elevation: MaterialStateProperty.all(10),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          )),
-                          fixedSize: MaterialStateProperty.all(Size(180, 70)),
-                        ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 35,
-                      ),
-                      const SizedBox(height: 3.0),
-                      Text(
-                        'ToDo erstellen',
-                        style: standardTextDecoration.copyWith(fontSize: 17),
-                      ),
-                    ],
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              CustomContainer(taskList: taskList),
+              const SizedBox(
+                height: 2,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Expanded(
+                  child: ListOfTasks(
+                listDescription: widget.taskList.description,
+                lists: widget.lists,
+              )),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  getEditButton(taskList, _database, context),
+                  const SizedBox(
+                    width: 25,
                   ),
-                ),
-                const SizedBox(
-                  width: 25,
-                ),
-                getDeleteButton(_database, taskList, context)
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            )
-          ],
+                  ElevatedButton(
+                    onPressed: () {
+                      _database.getAvailableListForUser().then((lists) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => CreateToDo(
+                                    listCreatedFrom: taskList.description,
+                                    availableLists: lists))));
+                      });
+                      setState(() {});
+                    },
+                    style: ButtonStyle(
+                            padding: MaterialStateProperty.all(EdgeInsets.all(1)),
+                            surfaceTintColor: MaterialStateProperty.all(
+                                AppColors.myCheckItGreen),
+                            overlayColor: MaterialStateProperty.all(
+                                AppColors.myCheckItGreen),
+                            backgroundColor: MaterialStateProperty.all(AppColors.myCheckItGreen),
+                            elevation: MaterialStateProperty.all(10),
+                            shape:
+                                MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            )),
+                            fixedSize: MaterialStateProperty.all(Size(180, 70)),
+                          ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 35,
+                        ),
+                        const SizedBox(height: 3.0),
+                        Text(
+                          'ToDo erstellen',
+                          style: standardTextDecoration.copyWith(fontSize: 17),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  getDeleteButton(_database, taskList, context)
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
+
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({
+    super.key,
+    required this.taskList,
+  });
+
+  final TaskList taskList;
+
+  @override
+  Widget build(BuildContext context) {
+    final tt = Provider.of<List<TaskList>>(context).where((element) => element.listReference == taskList.listReference);
+    if(tt.isEmpty){
+      print("hi");
+    }else{
+      print(tt.first.description);
+    }
+    return tt.isEmpty ? Loading() : Container(
+      alignment: Alignment.topLeft,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 8, 8, 8),
+        child: Text(
+          tt.first.description,
+          style: const TextStyle(
+              color: AppColors.myTextColor, fontSize: 25),
         ),
       ),
     );
